69b65d9a90ac36b88e06c3274992af64fd09c666
Elizabeth Duenas <salgadoe88@gmail.com>
2019-09-21 23:38:01 -0500

    updates
    updates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,10 +1,9 @@
 //from data.js
 let tableData = data;
 var tbody = d3.select("tbody");
-var button = d3.select("#filter-btn");
 // YOUR CODE HERE!
 ​
-function newTable(data){
+function newTable(data) {
   tbody.html("");
   data.forEach((sighting) => {
     var row = tbody.append("tr");
@@ -15,20 +14,19 @@ function newTable(data){
   });
 };
 
-​
+var button = d3.select("#filter-btn");
 button.on("click", function() {
+  d3.event.preventDefault();
 ​
-   d3.event.preventDefault();
-​
-    var inputElement = d3.select("#datetime");
-    var inputValue = inputElement.property("value");
-    console.log(inputValue);
+var inputElement = d3.select("#datetime");
+var inputValue = inputElement.property("value");
+console.log(inputValue);
   if(inputValue){
     var filteredData = tableData.filter(d => d.datetime === inputValue);
     console.log(filteredData)
     newTable(filteredData)
-  
-  }
+  };
 });
+
 // Build Table with data.js 
  newTable(tableData);
