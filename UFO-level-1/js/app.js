a85f0c1e589133a460d28e5e5979df0fac7a7fd7
Elizabeth Duenas <salgadoe88@gmail.com>
2019-09-21 22:43:14 -0500

    updates
    updates

--- UFO-level-1/js/app.js
+++ UFO-level-1/js/app.js
@@ -1,32 +1,36 @@
-// from data.js
+//from data.js
 let tableData = data;
-
-var tbody = d3.select("tbody"); 
-
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
 // YOUR CODE HERE!
-  data.forEach(sighting => {
+​
+function newTable(data){
+  tbody.html("");
+  data.forEach((sighting) => {
     var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key,value]) => {
+    Object.values(sighting).forEach((value) => {
       var cell = row.append("td");
       cell.text(value);
-      tbody.html(""); 
     });
   });
-
-
-var button = d3.select("#filter-btn");
+};
+​
+​
+​
+​
 button.on("click", function() {
-  d3.event.preventDefault(); 
-  var inputElement = d3.select("#datetime");
-  var inputValue = inputElement.property("value");
-
-  console.log(inputValue); 
-  console.log(filteredData)
-  var filteredData = tableData.filter(d => d.datetime === inputValue);
+​
+   d3.event.preventDefault();
+​
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+  if(inputValue){
+    var filteredData = tableData.filter(d => d.datetime === inputValue);
+    console.log(filteredData)
+    newTable(filteredData)
   
-}); 
-
-
-
-
- 
+  }
+});
+// Build Table with data.js 
+ newTable(tableData);
